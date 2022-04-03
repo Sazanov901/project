5ce88d15793105d6d2d1dee504bae64cd87748d5
Oleg <olegsazanov901@gmail.com>
2022-04-03 09:27:29 +0300

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//alert("hello")
+
+//const result = confirm("are you gay?");
+//console.log(result);
+
+//const answer = prompt("are you gay?", "gay");
+//console.log(answer);
+
+//const answers = [];
+//answers[0] = prompt("gay?");
+//answers[1] = prompt("are you sure?");
+//answers[2] = prompt("you are old gay?");
+
+//document.write(answers, " ");
+//document.write(typeof answers);
+
+//const category = "toys";
+//console.log(`https://toys.com${category}/5`);
+
+//const user = "gayyy";
+//alert(`hello, ${user}`);
+
+//console.log("arr" + " - object");
+//console.log(4 + " - object");
+//console.log(4 + + "5");
+
+//let incr = 10,
+
+// decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(--incr);
+//console.log(++decr);
+
+//console.log(5 % 2);
+
+//console.log(2 * 4 == "8");
+//console.log(2 * 4 === 8);
+
+//const isChecked = true,
+//  isClose = false;
+//console.log(isChecked && isClose); // или
+
+//const isChecked = true,
+//  isClose = false;
+//console.log(isChecked || isClose); // и
